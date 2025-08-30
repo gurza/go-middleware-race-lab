7e2471aec544c0b9cc19fc4b6fbd5122ccc89886
Sergei Popinevskii <gurza000@gmail.com>
2025-08-30 10:54:06 +0300

    feat: get rw from context

---
+++ cmd/exp2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"net/http"
+)
+
+type contextKey string
+
+const httpResponseWriterKey contextKey = "httpResponseWriter"
+
+func withResponseWriter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), httpResponseWriterKey, w)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wx := r.Context().Value(httpResponseWriterKey).(http.ResponseWriter)
+		http.SetCookie(wx, &http.Cookie{
+			Name:  "exp2",
+			Value: fmt.Sprintf("%d", rand.IntN(1_000_000)),
+			Path:  "/",
+		})
+
+		w.Write([]byte("kek"))
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("GET /", withResponseWriter(handler()))
+
+	fmt.Print("http://localhost:3001\n")
+	err := http.ListenAndServe(":3001", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
