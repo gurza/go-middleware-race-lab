58e4c4fa99218f7c03231e14328d9b67dcbdff04
Sergei Popinevskii <gurza000@gmail.com>
2025-08-30 09:29:05 +0300

    fix: write fixed mw constructor

--- cmd/ex01/main.go
+++ cmd/ex01/main.go
@@ -6,8 +6,32 @@ import (
 	"strings"
 )
 
+// go compiler transforms the closure into
+//
+//	type ClosureEnv struct {
+//		rateLimitEnabled *bool
+//		handler          *http.Handler
+//	}
+//
+//	func rateLimitMiddleware(w http.ResponseWriter, r *http.Request, env *ClosureEnv) {...}
 func NewMiddleware(handler http.Handler, rateLimitEnabled bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/admin") {
+			rateLimitEnabled = false
+		}
+		if rateLimitEnabled {
+			fmt.Printf("path=%s rate_limit_enabled=yes\n", r.URL.Path)
+		} else {
+			fmt.Printf("path=%s rate_limit_enabled=no\n", r.URL.Path)
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func NewMiddlewareFix(handler http.Handler, rateLimitEnabled bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rateLimitEnabled := rateLimitEnabled // copy to a request-local
+
 		if strings.HasPrefix(r.URL.Path, "/admin") {
 			rateLimitEnabled = false
 		}
--- cmd/ex01/main_test.go
+++ cmd/ex01/main_test.go
@@ -7,9 +7,21 @@ import (
 	"testing"
 )
 
-func TestMiddlewareDataRace(t *testing.T) {
+func TestMiddlewareRace_Racy(t *testing.T) {
+	testMiddleware(t, NewMiddleware)
+}
+
+func TestMiddlewareRace_Fixed(t *testing.T) {
+	testMiddleware(t, NewMiddlewareFix)
+}
+
+type mwFactory func(http.Handler, bool) http.Handler
+
+func testMiddleware(t *testing.T, makeMW mwFactory) {
+	t.Helper()
+
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
-	m := NewMiddleware(h, true)
+	m := makeMW(h, true)
 
 	var wg sync.WaitGroup
 	start := make(chan struct{})
